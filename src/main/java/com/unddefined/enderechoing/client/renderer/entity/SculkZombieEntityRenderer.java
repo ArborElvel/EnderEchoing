163fa2a68a731b2cf84bd4df74f66df0708e9daa
unddefined <669483446@qq.com>
2026-09-11 14:39:21 +0800

    Feat(Renderer): 让 sculk_zombie 头顶的触须直接用原版幽匿感测体的触须贴图渲染

--- src/main/java/com/unddefined/enderechoing/client/renderer/entity/SculkZombieEntityRenderer.java
+++ src/main/java/com/unddefined/enderechoing/client/renderer/entity/SculkZombieEntityRenderer.java
@@ -1,6 +1,7 @@
 package com.unddefined.enderechoing.client.renderer.entity;
 
 import com.unddefined.enderechoing.client.model.entity.SculkZombieEntityModel;
+import com.unddefined.enderechoing.client.renderer.layer.SculkZombieTendrilLayer;
 import com.unddefined.enderechoing.entities.SculkZombieEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
@@ -8,5 +9,6 @@ import software.bernie.geckolib.renderer.GeoEntityRenderer;
 public class SculkZombieEntityRenderer extends GeoEntityRenderer<SculkZombieEntity> {
     public SculkZombieEntityRenderer(EntityRendererProvider.Context c) {
         super(c, new SculkZombieEntityModel<>());
+        addRenderLayer(new SculkZombieTendrilLayer(this));
     }
 }
